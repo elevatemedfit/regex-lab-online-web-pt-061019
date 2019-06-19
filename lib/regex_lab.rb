d56365bab007b6e6a7274ea8c800adb4c85d9683
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-19 13:41:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-   if word.scan(/\w+[aeiou]/)
-     return true
-   elsif word.scan(/\w+[bcdfghj-np-tvwxz]/)
-     return !true
-
+   if word.scan(/\w+[aeiou]/)==true || word.scan(/\w+[bcdfghj-np-tvwxz]/)==false
 end
 end
 
